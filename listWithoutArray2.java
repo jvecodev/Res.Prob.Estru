a341729d3f89a4d8d8329b43d9fd5e2cda7d65bd
jvecodev <jvitor.oliveira1803@gmail.com>
2026-03-12 10:33:58 -0300

    refactor: reorganizar a classe listWithoutArray2 e implementar métodos para adicionar e exibir dados

--- listWithoutArray2.java
+++ listWithoutArray2.java
@@ -1,14 +1,48 @@
 public class listWithoutArray2 {
-    public static void main(String[] args) {
-        String[] pais = new String [5];
-        pais[0] = "Brasil";
-        pais[1] = "Argentina";
-        pais[2] = "Colombia";
-        pais[3] = "Peru";
-        pais[4] = "Venezuela";
 
-        for (int i = 0; i < pais.length; i++) {
-            System.out.println(pais[i]);
+    // This is the smae thing that array makes internal
+    // When complet the size of the array
+    // Fit in other place in memory with double the size
+    // Copy the data to bigger array
+
+    private int size;
+    private int[] data;
+
+    public listWithoutArray2(){
+        data = new int[2];
+        size = 0;
+    }
+
+    public void addData(int value){
+        if (size == data.length){
+            int [] newData = new int [data.length * 2];
+
+            for (int i = 0; i < data.length; i++) {
+                newData[i] = data[i];
+            }
+
+            data = newData;
         }
+
+        data[size] = value;
+        size++;
+    }
+
+    public void show(){
+        for (int q = 0; q < size; q++) {
+            System.out.println(data[q]);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        listWithoutArray2 list = new listWithoutArray2();
+
+        list.addData(1);
+        list.addData(2);
+        list.addData(3);
+        list.addData(4);
+
+        list.show();
     }
 }
